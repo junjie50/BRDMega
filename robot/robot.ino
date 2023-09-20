3579247b32c3ce559071d20761f6a02ed7fb57e7
= <=>
2023-09-20 17:26:58 +0800

    Tested up to FSM at second tray

--- robot/robot.ino
+++ robot/robot.ino
@@ -6,17 +6,26 @@ const String FEED_RATE = "F1000";
 const int SENSOR1 = 30; // Sensor at the start
 const int SENSOR2 = 31;
 const int SENSOR3 = 32;
+const int SENSOR4 = 33; // Sensor at the start
+const int SENSOR5 = 34;
+const int SENSOR6 = 35;
 
 // Motors
-const char motor1 = 'X';
-const char motor2 = 'Y';
+const char MOTOR1 = 'X';
+const char MOTOR2 = 'Y';
+const int X_DIR = 1;
+const int Y_DIR = 1;
+const double X_AMT = 0.1 * X_DIR;
 
-// Distance
-const int dist_tray_1 = 250;
 
 // Logical Flags
-bool ENTRY1 = false;
-bool EXIT1 = false;
+bool idleState = true;
+bool entry1 = false;
+bool exit1 = true;
+bool entry2 = false;
+bool exit2 = true;
+bool entry3 = false;
+bool exit3 = true;
 
 
 byte x;
@@ -27,7 +36,14 @@ String cmd;
 void grbl_update();
 void motion_stop();
 void FSM();
-void move_motor_res(char motor, int amount);
+void x_jog(double amount);
+void motor_jog(char motor, double amount);
+void test();
+void idle();
+void session();
+void reset_first_tray();
+void rack1_fsm();
+void rack2_fsm();
 
 void setup() {
   // put your setup code here, to run once:
@@ -39,38 +55,95 @@ void setup() {
   // Serial 1 talks with the blackbox x32
   Serial1.begin(115200);
 
-  pinMode(sensor1, INPUT);
-  pinMode(sensor2, INPUT);
-  pinMode(sensor3, INPUT);
+
+  //
+  pinMode(SENSOR1, INPUT);
+  pinMode(SENSOR2, INPUT);
+  pinMode(SENSOR3, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Currently testing the motor.
-
-  // Testing the sensor part
-  if(!digitalRead(sensor1)) {
-    Serial.println("Sensor 1 detected, moving motor.");
-    move_motor_res('X', -0.1);
-  }
-
-  if(!digitalRead(sensor2)) {
-    Serial.println("Sensor 2 detected, stopping motor.");
-    motion_stop();
-  }
+  FSM();
 }
 
 // Creating the logic for the machine.
 void FSM() {
-  
+  if(idleState) {
+    idle();
+  }
+  else {
+    session();
+  }
 }
 
-void move_motor_res(char motor, double amount) {
-  String sign = amount < 0?"-":"+";
-  String ops = MOVE_RES + String(motor);
-  ops += (sign + String(abs(amount), 2) + FEED_RATE + "\n");
-  Serial.println(ops);
+// Code flow for idle state to prevent balooned code.
+// Idle state to check for detection of sensors/robot or from rack.
+void idle() {
+  if(!digitalRead(SENSOR1)) {
+    entry1 = true;
+    exit1 = false;
+    idleState = false;
+  }
+  else{
+    delay(100);
+  }
+  // Add in other sensor triggers
+}
 
+// Code flow for when object has been detected.
+void session() {
+  if(entry1) {
+    Serial.println("In first tray");
+    rack1_fsm();
+  }
+  else if (entry2) {
+    Serial.println("In second tray");
+    rack2_fsm();
+  }
+}
+
+// rack1 FSM
+
+void rack1_fsm() {
+  if(!exit1) {
+    x_jog(X_AMT);
+    if(!digitalRead(SENSOR2)) {
+      exit1 = true;
+    }
+  }
+  else{
+    if(!entry2) {
+      x_jog(X_AMT);
+      if(!digitalRead(SENSOR3)) {
+        entry2 = true;
+        reset_first_tray();
+      }
+    }
+  }
+}
+
+// rack1 FSM Second tray
+void rack2_fsm() {
+  //
+}
+
+
+void reset_first_tray() {
+  entry1 = false;
+  exit1 = true;
+}
+
+
+//##########################MOTION CONTROL##########################//
+void x_jog(double amount) {
+  motor_jog(MOTOR1, amount);
+}
+
+void motor_jog(char motor, double amount) {
+  String ops = MOVE_RES + String(motor);
+  String sign = amount < 0?"-":"+";
+  ops += (sign + String(abs(amount), 2) + FEED_RATE + "\n");
+  //Serial.println(ops);
   // Calling for the motor to move
   Serial1.print(ops);
 }
@@ -78,7 +151,6 @@ void move_motor_res(char motor, double amount) {
 void motion_stop() {
   String ops ="!\n";
   Serial.println(ops);
-  // Calling for the motor to stop
   Serial1.write(133);
 }
 
@@ -87,5 +159,24 @@ void grbl_update() {
   if (Serial1.available()) {
     String grblMSG = Serial1.readString();
     Serial.println(grblMSG);
+  }
+}
+
+
+
+//##########################TESTING##########################//
+void test() {
+  // put your main code here, to run repeatedly:
+  // Currently testing the motor.
+
+  // Testing the sensor part
+  if(!digitalRead(SENSOR1)) {
+    Serial.println("Sensor 1 detected, moving motor.");
+    motor_jog('X', -0.1);
+  }
+
+  if(!digitalRead(SENSOR2)) {
+    Serial.println("Sensor 2 detected, stopping motor.");
+    motion_stop();
   }
 }
